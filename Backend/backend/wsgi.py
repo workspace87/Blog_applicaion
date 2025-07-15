6070e06baabbf6e412cba187e16264c01ed656fc
workspace87 <prathmeshkorde89@gmail.com>
2025-07-15 23:10:23 +0530

    wsgi

--- Backend/backend/wsgi.py
+++ Backend/backend/wsgi.py
@@ -1,15 +1,13 @@
-"""
-WSGI config for Backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+# This 'path' resolves to blog_backend/ (the parent of backend/)
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+# ***CHANGE THIS LINE TO MATCH THE RENAMED FOLDER:***
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings") # Change 'Backend' to 'backend'
 application = get_wsgi_application()
