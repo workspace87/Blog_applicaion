53cbd13a3979f9559837bdd2e6b9fb5394f04474
workspace87 <prathmeshkorde89@gmail.com>
2025-07-15 23:50:05 +0530

    new commit

--- Backend/backend/wsgi.py
+++ Backend/backend/wsgi.py
@@ -9,5 +9,5 @@ if path not in sys.path:
     sys.path.insert(0, path)
 
 # ***CHANGE THIS LINE TO MATCH THE RENAMED FOLDER:***
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Backend.settings") # Change 'Backend' to 'backend'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings") # Change 'Backend' to 'backend'
 application = get_wsgi_application()
